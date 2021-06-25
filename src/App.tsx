4fb898c3ea4309de6f42cb037b5369dcebe18546
Inna3112 <innula3112@gmail.com>
2021-06-25 11:24:08 +0300

    Updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,6 @@ import {AppRootStateType} from "./store/store";
 
 function App() {
 
-    // useEffect(() => {
-    //     let numAsString = localStorage.getItem('num')
-    //     if(numAsString){
-    //         let newNum = JSON.parse(numAsString)
-    //         setNum(newNum)
-    //     }
-    //     let minValueAsString = localStorage.getItem('minValue')
-    //     if(minValueAsString){
-    //         let newMinValue = JSON.parse(minValueAsString)
-    //         setMinValue(newMinValue)
-    //     }
-    //     let maxValueAsString = localStorage.getItem('maxValue')
-    //     if(maxValueAsString){
-    //         let newMaxValue = JSON.parse(maxValueAsString)
-    //         setMaxValue(newMaxValue)
-    //     }
-    //
-    // }, [])
-
-    // useEffect(()=>{
-    //     localStorage.setItem('num', JSON.stringify(num))
-    //     localStorage.setItem('minValue', JSON.stringify(minValue))
-    //     localStorage.setItem('maxValue', JSON.stringify(maxValue))
-    // }, [num, minValue, maxValue])
-
     const mode = useSelector<AppRootStateType, boolean>(state => state.counter['mode'])
 
     return (
